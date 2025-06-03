10203e66229a22a3f49fee617bfd6c6391116489
LAPTOP-SGVCVDCM\Hiranya <iduwara123lk@gmail.com>
2025-06-03 15:07:00 +0100

    New folder making

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def check_int(user_input):
         return True
     except ValueError:
         return False
+    
 
 print("Welcome to File Manager")
 
@@ -26,23 +27,35 @@ user_option=input("Your option : ")
 if check_int(user_option):
     user_option = int(user_option)
     if (user_option==1):
-        file_path=r"C:\Users\Hiranya\Desktop"
-        files =os.listdir(file_path)
 
-        items = os.listdir(file_path)
+        
+        # file_path=r"C:\Users\Hiranya\Desktop"
+        file_path = input("Please enter file path: ")
+        file_path = file_path.replace("\\", "/")
 
+        print(file_path)
 
-        for item in items:
-            item_path = os.path.join(file_path, item)  # Combine path with item
-            if os.path.isfile(item_path):
-                print(f"File: {item}")
-            elif os.path.isdir(item_path):
-                print(f"Folder: {item}")
-            else:
-                print(f"Unknown item: {item}")
+        if os.path.exists(file_path):
+            items = os.listdir(file_path)
+
+            for item in items:
+                item_path = os.path.join(file_path, item)
+                if os.path.isfile(item_path):
+                    print(f"File: {item}")
+                elif os.path.isdir(item_path):
+                    print(f"Folder: {item}")
+                else:
+                    print(f"Unknown item: {item}")
+        else:
+            print("Please enter a valid file path")
 
     elif(user_option==2):
-        print("Create a folder")
+        #C:\Users\Hiranya\Desktop\file_handler\file_organizer
+        folder_location=input("Please enter your new folder path: ")
+        folder_location=folder_location.replace("\\", "/")
+        os.mkdir(folder_location)
+        print("Successfully created a new folder.")
+
 
     elif (user_option==3):
         print("Delete a folder")
